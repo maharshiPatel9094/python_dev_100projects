4cc2aa7485cb0d07d56a2a25a7ea58daf7390707
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-09-27 11:20:51 -0400

    decrypting process completed

---
+++ caesar_cypher/decrypt.py
@@ -0,0 +1,10 @@
+def decrypt(original_text,shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+    print(f"Here is the de coded result: {cipher_text}")
+
+
+decrypt(original_text,shift_amount)
